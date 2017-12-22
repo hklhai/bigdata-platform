fe30ef41a7f97965c1149d1e7d7fd740b15eb0aa
hklhai <hkhai@outlook.com>
2017-12-22 08:31:59 +0800

    bigdata platform framework

---
+++ README.md
@@ -0,0 +1,11 @@
+## bigdata-platform
+hxqh-bigdata-platform
+
+---
+> HK  
+> linh@bjhxqh.com
+
+
+> 小豆苗  
+> aiym@bjhxqh.com
+
---
+++ src/main/java/com/hxqh/bigdata/conf/ConfigurationManagerTest.java
@@ -0,0 +1,15 @@
+package com.hxqh.bigdata.conf;
+
+/**
+ * 配置管理组件测试类
+ */
+public class ConfigurationManagerTest {
+
+    public static void main(String[] args) {
+        String testkey1 = ConfigurationManager.getProperty("testkey1");
+        String testkey2 = ConfigurationManager.getProperty("testkey2");
+        System.out.println(testkey1);
+        System.out.println(testkey2);
+    }
+
+}
